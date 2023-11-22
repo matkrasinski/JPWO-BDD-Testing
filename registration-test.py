409b9b84658a5e4a41937168d3a9eba49bb26edd
matkrasinski <mat.krasinski@o2.pl>
2023-11-22 18:51:49 +0100

    Refactoring

--- registration-test.py
+++ registration-test.py
@@ -14,6 +14,7 @@ from faker import Faker
 
 class RegistrationTest(unittest.TestCase):
     
+    fake = Faker()
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -26,9 +27,10 @@ class RegistrationTest(unittest.TestCase):
         self.base_url = "https://practicesoftwaretesting.com"
         self.driver.get(self.base_url)
         self.wait = WebDriverWait(self.driver, 10)
+        
 
-    def generate_random_date_of_birth(self, fake):
-        random_age = fake.random_int(min=18, max=65)
+    def generate_random_date_of_birth(self, min_age=18, max_age=100):
+        random_age = Faker().random_int(min=min_age, max=max_age)
         
         birth_date = datetime.now() - timedelta(days=random_age * 365)
         
@@ -42,29 +44,73 @@ class RegistrationTest(unittest.TestCase):
         )
         return element.is_displayed() and element.text == "Login"
 
+    def registration_error_is_displayed(self):
+        element = WebDriverWait(self.driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, "app-register form.ng-dirty.ng-touched.ng-valid.ng-submitted .alert.alert-danger .help-block"))
+        )
+        return element.is_displayed()
+
+
+    def go_to_login_page(self):
+        return WebDriverUtils(driver=self.driver)\
+            .click_by_css_selector(css_selector='a[data-test="nav-sign-in"]')
+
+    def register_from_login_page(self,
+                                web_driver_utils=None,
+                                first_name=fake.first_name(),
+                                last_name=fake.last_name(),
+                                dob=None,
+                                address=fake.address(),
+                                postcode=fake.postcode(),
+                                city=fake.city(),
+                                state=fake.state(),
+                                phone_number=''.join(char for char in fake.phone_number() if char.isdigit()),
+                                email=fake.email(),
+                                password=fake.password(),
+                                seconds=1
+                                ):
+        dob = dob or self.generate_random_date_of_birth()
+        web_driver_utils = web_driver_utils or WebDriverUtils(driver=self.driver)
+
+        web_driver_utils\
+            .click(xpath="//div[@class='col-lg-6 auth-form']//form//div[@class='input-group mb-3'][4]//p//a[1]")\
+            .fill(xpath='//*[@id="first_name"]', value=first_name)\
+            .fill(xpath='//*[@id="last_name"]', value=last_name)\
+            .fill(xpath='//*[@id="dob"]', value=dob)\
+            .fill(xpath='//*[@id="address"]', value=address)\
+            .fill(xpath='//*[@id="postcode"]', value=postcode)\
+            .fill(xpath='//*[@id="city"]',value=city)\
+            .fill(xpath='//*[@id="state"]', value=state)\
+            .select_option(xpath='//*[@id="country"]', index=42)\
+            .fill(xpath='//*[@id="phone"]', value=phone_number)\
+            .fill(xpath='//*[@id="email"]', value=email)\
+            .fill(xpath='//*[@id="password"]', value=password)\
+            .click_and_sleep(xpath='//div[@class="col-lg-8 auth-form"]//form//button[@class="btnSubmit mb-3"]', seconds=seconds)
+
 
     def test_user_registration(self):
-        fake = Faker()
-        WebDriverUtils(self.driver)\
-            .click_by_css_selector(css_selector='a[data-test="nav-sign-in"]')\
-            .click(xpath="//div[@class='col-lg-6 auth-form']//form//div[@class='input-group mb-3'][4]//p//a[1]")\
-            .fill(xpath='//*[@id="first_name"]', value=fake.first_name())\
-            .fill(xpath='//*[@id="last_name"]', value=fake.last_name())\
-            .fill(xpath='//*[@id="dob"]', value=self.generate_random_date_of_birth(fake))\
-            .fill(xpath='//*[@id="address"]', value=fake.address())\
-            .fill(xpath='//*[@id="postcode"]', value=fake.postcode())\
-            .fill(xpath='//*[@id="city"]',value=fake.city())\
-            .fill(xpath='//*[@id="state"]', value=fake.state())\
-            .select_option(xpath='//*[@id="country"]', index=42)\
-            .fill(xpath='//*[@id="phone"]', value=''.join(char for char in fake.phone_number() if char.isdigit()))\
-            .fill(xpath='//*[@id="email"]', value=fake.email())\
-            .fill(xpath='//*[@id="password"]', value=fake.password())\
-            .click(xpath='//div[@class="col-lg-8 auth-form"]//form//button[@class="btnSubmit mb-3"]')
-
+        web_driver_utils = self.go_to_login_page()
+        self.register_from_login_page(web_driver_utils=web_driver_utils)
         assert self.login_page_is_displayed(), "User is not on the login page after submission."
 
+
+    def test_duplicate_email_registration(self):
+        email = Faker().email()
+        web_driver_utils = self.go_to_login_page()
+        self.register_from_login_page(web_driver_utils=web_driver_utils, email=email)
+        assert self.login_page_is_displayed(), "User is not on the login page after submission."
+
+        self.register_from_login_page(web_driver_utils=web_driver_utils, email=email)
+        assert self.registration_error_is_displayed(), "Somehow the user was able to register with used email"
+
+
+    def test_not_valid_dob(self):
+        web_driver_utils = self.go_to_login_page()
+        self.register_from_login_page(web_driver_utils=web_driver_utils, dob=self.generate_random_date_of_birth(min_age=10, max_age=11))
+        assert self.registration_error_is_displayed(), "User is too young to register an account."
+
+
     def tearDown(self):
-        time.sleep(10)
         self.driver.quit()
 
 if __name__ == "__main__":
--- test/test_product.py
+++ test/test_product.py
@@ -23,22 +23,22 @@ class ExampleTest(unittest.TestCase):
         self.wait = WebDriverWait(self.driver, 10)
 
     def test_add_product(self):
-        web_driver_utils = WebDriverUtils(self.driver)\
+        web_driver_utils = WebDriverUtils(driver=self.driver)\
             .login_as_admin()\
-            .click('//*[@id="admin-menu"]')\
-            .click("/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
-            .click("/html/body/app-root/div/app-products-list/form/div/a")\
-            .fill('//*[@id="name"]', "Test Product")\
-            .fill('//*[@id="description"]', "Test Product Description")\
-            .fill('//*[@id="stock"]', "10")\
-            .fill('//*[@id="price"]', "123.45")\
-            .select_option('//*[@id="brand_id"]', 0)\
-            .select_option('//*[@id="category_id"]', 0)\
-            .select_option('//*[@id="product_image_id"]', 0)\
-            .click('/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')\
-            .click("/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/a")\
-            .fill("/html/body/app-root/div/app-products-list/form/div/input", "Test Product")\
-            .click_and_sleep( "/html/body/app-root/div/app-products-list/form/div/button[1]", 1)
+            .click(xpath='//*[@id="admin-menu"]')\
+            .click(xpath="/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
+            .click(xpath="/html/body/app-root/div/app-products-list/form/div/a")\
+            .fill(xpath='//*[@id="name"]', value="Test Product")\
+            .fill(xpath='//*[@id="description"]', value="Test Product Description")\
+            .fill(xpath='//*[@id="stock"]', value="10")\
+            .fill(xpath='//*[@id="price"]', value="123.45")\
+            .select_option(xpath='//*[@id="brand_id"]', index=0)\
+            .select_option(xpath='//*[@id="category_id"]', index=0)\
+            .select_option(xpath='//*[@id="product_image_id"]', index=0)\
+            .click(xpath='/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')\
+            .click(xpath="/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/a")\
+            .fill(xpath="/html/body/app-root/div/app-products-list/form/div/input", value="Test Product")\
+            .click_and_sleep(xpath="/html/body/app-root/div/app-products-list/form/div/button[1]", seconds=1)
 
         # The product is not added, due to a bug in the application.
         self.assertTrue(
@@ -49,40 +49,40 @@ class ExampleTest(unittest.TestCase):
         )
 
     def test_edit_product_with_negative_price(self):
-        web_driver_utils = WebDriverUtils(self.driver)\
+        web_driver_utils = WebDriverUtils(driver=self.driver)\
             .login_as_admin()\
-            .click('//*[@id="admin-menu"]')\
-            .click("/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")
+            .click(xpath='//*[@id="admin-menu"]')\
+            .click(xpath="/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")
 
         product_id = web_driver_utils.get_element_text("/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td[1]")
 
         web_driver_utils\
-            .click_and_sleep("/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td[5]/a", 2)\
-            .fill('//*[@id="price"]', "-123.45")\
-            .click('/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')\
-            .click("/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/a")
+            .click_and_sleep(xpath="/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td[5]/a", seconds=2)\
+            .fill(xpath='//*[@id="price"]', value="-123.45")\
+            .click(xpath='/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')\
+            .click(xpath="/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/a")
         
-        product_modified_price = web_driver_utils.get_element_text(f'//tr[td[text()="{product_id}"]]/td[4]')
+        product_modified_price = web_driver_utils.get_element_text(xpath=f'//tr[td[text()="{product_id}"]]/td[4]')
 
         self.assertEqual("$14.15", product_modified_price)
 
         # Restore original price to prevent the next test failure
-        web_driver_utils.click_and_sleep("/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td[5]/a", 2)\
-            .fill('//*[@id="price"]', "14.15")\
-            .click('/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')
+        web_driver_utils.click_and_sleep(xpath="/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td[5]/a", seconds=2)\
+            .fill(xpath='//*[@id="price"]', value="14.15")\
+            .click(xpath='/html/body/app-root/div/app-products-add-edit/div/form/div[3]/div/button')
 
     def test_display_product_details(self):
-        web_driver_utils = WebDriverUtils(self.driver)\
+        web_driver_utils = WebDriverUtils(driver=self.driver)\
             .login_as_admin()\
-            .click('//*[@id="admin-menu"]')\
-            .click("/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
-            .fill("/html/body/app-root/div/app-products-list/form/div/input", "Slip Joint Pliers")\
-            .click("/html/body/app-root/div/app-products-list/form/div/button[1]")
+            .click(xpath='//*[@id="admin-menu"]')\
+            .click(xpath="/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
+            .fill(xpath="/html/body/app-root/div/app-products-list/form/div/input", value="Slip Joint Pliers")\
+            .click(xpath="/html/body/app-root/div/app-products-list/form/div/button[1]")
 
         base_xpath = "/html/body/app-root/div/app-products-list/table/tbody/tr[1]/td"
-        name = web_driver_utils.get_element_text(f"{base_xpath}[2]")
-        stock = web_driver_utils.get_element_text(f"{base_xpath}[3]")
-        price = web_driver_utils.get_element_text(f"{base_xpath}[4]")
+        name = web_driver_utils.get_element_text(xpath=f"{base_xpath}[2]")
+        stock = web_driver_utils.get_element_text(xpath=f"{base_xpath}[3]")
+        price = web_driver_utils.get_element_text(xpath=f"{base_xpath}[4]")
 
         # Validate product details
         self.assertEqual("Slip Joint Pliers", name)
@@ -90,12 +90,12 @@ class ExampleTest(unittest.TestCase):
         self.assertEqual("$9.17", price)
 
     def test_display_empty_product_list(self):
-        web_driver_utils = WebDriverUtils(self.driver)\
+        web_driver_utils = WebDriverUtils(driver=self.driver)\
             .login_as_admin()\
-            .click('//*[@id="admin-menu"]')\
-            .click("/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
-            .fill("/html/body/app-root/div/app-products-list/form/div/input", "Non Existing Product")\
-            .click("/html/body/app-root/div/app-products-list/form/div/button[1]")
+            .click(xpath='//*[@id="admin-menu"]')\
+            .click(xpath="/html/body/app-root/app-header/nav/div/div/ul/li[4]/ul/li[4]/a")\
+            .fill(xpath="/html/body/app-root/div/app-products-list/form/div/input", value="Non Existing Product")\
+            .click(xpath="/html/body/app-root/div/app-products-list/form/div/button[1]")
 
         # Validate product details
         self.assertFalse(
